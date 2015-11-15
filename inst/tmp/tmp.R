837237de28f4f66b8374c174ed99407f60338c34
jpmarindiaz <jpmarindiaz@gmail.com>
2015-11-14 23:04:33 -0500

    new maps

--- inst/tmp/tmp.R
+++ inst/tmp/tmp.R
@@ -14,7 +14,7 @@ map_wgr84 <- spTransform(map, CRS=CRS("+init=EPSG:4326"))
 writeOGR(map_wgr84, "tmp.json", "tmp",driver='GeoJSON')
 
 map_2 <- spTransform(map, CRS=CRS("+proj=utm +zone=32 +datum=WGS84 +units=m +no_defs"))
-geojsonio::geojson_write(map_2, file = "tmp2.geojson")
+geojsonio::geojson_write(map_2, file = "inst/tmp/tmp2.geojson")
 
 
 
