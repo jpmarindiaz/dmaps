6990b20daf34050217112b297763b2a22a6f8de7
jpmarindiaz <jpmarindiaz@gmail.com>
2017-07-28 10:59:46 -0500

    fixed preprocessData when code is null

--- R/dmaps-utils.R
+++ R/dmaps-utils.R
@@ -1,10 +1,12 @@
 
 preprocessData <- function(data, mapName){
-  if(mapName == "col_municipalities"){
-    data$code <- sprintf("%05d", as.numeric(data$code))
-  }
-  if(mapName == "col_departments"){
-    data$code <- sprintf("%02d", as.numeric(data$code))
+  if(!is.null(data$code)){
+    if(mapName == "col_municipalities"){
+      data$code <- sprintf("%05d", as.numeric(data$code))
+    }
+    if(mapName == "col_departments"){
+      data$code <- sprintf("%02d", as.numeric(data$code))
+    }
   }
   data
 }
